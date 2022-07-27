1a6ea5ea8795a0a8014b6b4291f7c6190ac3ea88
balemger <b.alemger@gmail.com>
2022-07-27 22:39:21 +0400

    Added CodeSniffer

--- src/Cli.php
+++ src/Cli.php
@@ -5,7 +5,8 @@ namespace BrainGames\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function wellcome() {
+function welcome()
+{
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
