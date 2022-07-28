049651a2573fa635e7f5fdcbd35a581b35f545a7
balemger <b.alemger@gmail.com>
2022-07-28 18:04:21 +0400

    Step 8. Brain Progression Fixed

--- src/Engine.php
+++ src/Engine.php
@@ -11,7 +11,7 @@ const MAX_VALUE = 100;
 
 function welcome()
 {
-    line('Welcome to the Brain Game!');
+    line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
     return $name;
