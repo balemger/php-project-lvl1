7694d3ef7daedd8267bde668d629b5a58e56c094
balemger <b.alemger@gmail.com>
2022-07-28 19:27:09 +0400

    Step 9. Fixed Brain Prime

--- src/games/Prime.php
+++ src/games/Prime.php
@@ -16,7 +16,26 @@ function startBrainPrime()
     for ($i = 0; $i < ITERATIONS; $i += 1) {
         $question = rand(MIN_VALUE, MAX_VALUE);
         $questionAnswers[$i]['question'] = $question;
-        $questionAnswers[$i]['answer'] = $question % 2 !== 0 ? 'yes' : 'no';
+        $questionAnswers[$i]['answer'] = isPrime($question) === true ? 'yes' : 'no';
     }
     startBrainGame(INSTRUCTION, $questionAnswers);
+}
+
+
+function isPrime($num)
+{
+    if ($num == 1) {
+        return false;
+    }
+    $bCheck = true;
+    $highestIntegralSquareRoot = floor(sqrt($num));
+    for ($i = 2; $i <= $highestIntegralSquareRoot; $i++)
+    {
+        if ($num % $i == 0)
+        {
+            $bCheck = false;
+            break;
+        }
+    }
+    return $bCheck;
 }
